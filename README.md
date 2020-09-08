d1deb1b6ee2a489822463f614047c885ec6391d7
Carlos Salinas <carlos.ariel.salinas@hotmail.com>
2020-09-08 15:56:34 -0300

    Proyecto en blanco

--- README.md
+++
@@ -1,1 +0,0 @@
-# ProyectoG2-Informatorio
--- turnos/turnos/asgi.py
+++ turnos/turnos/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for gestionComercial project.
+ASGI config for turnos project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gestionComercial.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'turnos.settings')
 
 application = get_asgi_application()
