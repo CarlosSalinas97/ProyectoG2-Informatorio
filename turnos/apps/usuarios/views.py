72e9de31b2ee46ef3d9b92519944bbee33e2791a
Carlos Salinas <carlos.ariel.salinas@hotmail.com>
2020-09-08 16:39:27 -0300

    App usuarios agregada

---
+++ turnos/apps/usuarios/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.views.generic import CreateView
+from .forms import RegistroUsuarioForm
+from .models import Usuario
+from django.urls import reverse_lazy
+
+# Create your views here.
