88cad99883c1e7653d01a6ccb6c5d3701572508d
Renzo Balbiano <balbianor@gmail.com>
2020-09-09 21:50:40 -0300

    cambios en el login
    Hice cambios para el inicio de sesión

--- turnos/turnos/urls.py
+++ turnos/turnos/urls.py
@@ -18,10 +18,13 @@ from django.urls import path,include
 from django.contrib.auth import views as auth
 from . import views
 from django.conf.urls.static import static
+
 from django.conf import settings
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',views.Home,name='home')
+    path('',views.Home,name='home'),
+    path('Login',auth.LoginView.as_view(template_name="usuarios/login.html"),name="login"),
+    path('Logout',auth.LogoutView.as_view(),name="logout"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
