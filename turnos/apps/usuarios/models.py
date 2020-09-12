0ef1f1b66847bb25a5cf290635e15a12124abb64
Franco Toñanes <adriantonanes@gmail.com>
2020-09-12 17:57:20 -0300

    Login con Email
    Modificamos el ingreso para que pueda ser realizado con el email

--- turnos/apps/usuarios/models.py
+++ turnos/apps/usuarios/models.py
@@ -4,4 +4,7 @@ from django.contrib.auth.models import AbstractUser
 # Create your models here.
 
 class Usuario(AbstractUser):
-    pass
+    email = models.EmailField('email address', unique=True)
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
