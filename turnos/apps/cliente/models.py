9465c1652a292e5f842d220f868bdf92c0544501
Argenthands <alejandro.david.benolol@gmail.com>
2020-09-18 14:27:59 +0300

    vista local

--- turnos/apps/cliente/models.py
+++ turnos/apps/cliente/models.py
@@ -3,4 +3,4 @@ from apps.usuarios.models import Usuario
 
 # Create your models here.
 class Cliente(models.Model):
-	usuario = models.OneToOneField(Usuario, related_name="usuario_cliente",on_delete=models.CASCADE)
+	usuario = models.OneToOneField(Usuario, related_name = "usuario_cliente", on_delete = models.CASCADE)
--- turnos/apps/empresa/forms.py
+++ turnos/apps/empresa/forms.py
@@ -1,1 +1,12 @@
-forms.py
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from apps.empresa.models import Empresa
+from django.db import transaction
+from .models import Cliente
+
+class EmpresaForm(UserCreationForm):
+
+
+	class Meta:
+		model = Empresa 
+		fields = ['CUIT', 'DNI', 'Nombre', 'Razon_Social', 'Superficie', 'Cant_empleados']
